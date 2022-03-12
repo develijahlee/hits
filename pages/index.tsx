3f86487f36c716dbcfbc659103792a3a1f19e8fd
Elijah Lee <develijahlee@gmail.com>
2022-03-12 16:09:47 +0900

    show UI data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
@@ -41,22 +40,27 @@ const Home: NextPage = () => {
         {loading ? (
           <div>Loading...</div>
         ) : (
-          <div>Hello World</div>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Foxtrot</th>
+                <th>Golf</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((d, i) => (
+                <tr key={i}>
+                  <td>{d[0]}</td>
+                  <td>{d[1]}</td>
+                  <td>{d[2]}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
         )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
